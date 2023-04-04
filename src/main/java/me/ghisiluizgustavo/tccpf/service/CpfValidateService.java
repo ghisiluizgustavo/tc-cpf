a131452791810ca2fd71c313f8f13fb37e0549eb
Luiz Gustavo Ghisi <ghisiluizgustavo@gmail.com>
2023-04-04 16:19:32 -0300

    feat: removed comments

--- src/main/java/me/ghisiluizgustavo/tccpf/service/CpfValidateService.java
+++ src/main/java/me/ghisiluizgustavo/tccpf/service/CpfValidateService.java
@@ -27,45 +27,38 @@ public class CpfValidateService {
         char dig10, dig11;
         int sm, i, r, num, peso;
 
-        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        sm = 0;
+        peso = 10;
+        for (i=0; i<9; i++) {
 
-            // Calculo do 1o. Digito Verificador
-            sm = 0;
-            peso = 10;
-            for (i=0; i<9; i++) {
-                // converte o i-esimo caractere do CPF em um numero:
-                // por exemplo, transforma o caractere '0' no inteiro 0
-                // (48 eh a posicao de '0' na tabela ASCII)
-                num = (int)(cpf.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso - 1;
-            }
+            num = (int)(cpf.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+        }
 
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11))
-                dig10 = '0';
-            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+        r = 11 - (sm % 11);
+        if ((r == 10) || (r == 11))
+            dig10 = '0';
+        else dig10 = (char)(r + 48); 
 
-            // Calculo do 2o. Digito Verificador
-            sm = 0;
-            peso = 11;
-            for(i=0; i<10; i++) {
-                num = (int)(cpf.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso - 1;
-            }
+        sm = 0;
+        peso = 11;
+        for(i=0; i<10; i++) {
+            num = (int)(cpf.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+        }
 
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11))
-                dig11 = '0';
-            else dig11 = (char)(r + 48);
+        r = 11 - (sm % 11);
+        if ((r == 10) || (r == 11))
+            dig11 = '0';
+        else dig11 = (char)(r + 48);
 
-            // Verifica se os digitos calculados conferem com os digitos informados.
-            if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10))){
-                cpfResponse.setStatus(CpfStatus.ABLE_TO_VOTE);
-                return cpfResponse;
-            }
-            cpfResponse.setStatus(CpfStatus.UNABLE_TO_VOTE);
+        if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10))){
+            cpfResponse.setStatus(CpfStatus.ABLE_TO_VOTE);
             return cpfResponse;
+        }
+        cpfResponse.setStatus(CpfStatus.UNABLE_TO_VOTE);
+        return cpfResponse;
     }
 }
